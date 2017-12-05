3ce09954ebb94dcf487da8816b021089826b4d5a
Zway <zway8083@gmail.com>
2017-12-05 16:51:42 +0100

    add SLL + dictation fix

---
+++ src/main/java/server/api/LoginController.java
@@ -0,0 +1,29 @@
+package server.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import server.database.model.User;
+import server.database.repository.UserRepository;
+import server.model.LoginForm;
+
+@Controller
+public class LoginController {
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostMapping(path = "/api/login")
+	public ResponseEntity<?> login(@RequestBody LoginForm loginForm) {
+		System.out.println(loginForm.toString());
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		User user = userRepository.findByEmail(loginForm.getUsername());
+		if (user == null || !encoder.matches(loginForm.getPassword(), user.getPassword()))
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
--- src/main/resources/static/scripts/dictation.js
+++ src/main/resources/static/scripts/dictation.js
@@ -1,23 +1,43 @@
-function startDictation(id) {
+var recognition = null;
 
-	if (window.hasOwnProperty('webkitSpeechRecognition')) {
-
+if (window.hasOwnProperty('webkitSpeechRecognition')) {
+	function initRecognition(inputId, button) {
 		var recognition = new webkitSpeechRecognition();
-
+		
 		recognition.continuous = false;
 		recognition.interimResults = false;
-
 		recognition.lang = "fr-FR";
-		recognition.start();
-
-		recognition.onresult = function(e) {
-			document.getElementById(id).value = e.results[0][0].transcript;
-			recognition.stop();
-		};
-
-		recognition.onerror = function(e) {
+		
+		recognition.onstart = function() {
+			document.getElementById(inputId).placeholder = "Parlez...";
+			button.style.background = "LimeGreen";
+		}
+		
+		recognition.onresult = function(event) {
+			document.getElementById(id).value = event.results[0][0].transcript;
 			recognition.stop();
 		}
-
+		
+		recognition.onerror = function(event) {
+			concole.error(event);
+		}
+		
+		recognition.onend = function() {
+			document.getElementById(inputId).placeholder = "";
+			button.style.background = "none";
+			recognition = null;
+		}
+	
+		return recognition;
 	}
+	
+	function dictation(inputId, button) {
+		if (recognition !== null) {
+			recognition.stop();
+		}
+		recognition = initRecognition(inputId, button);
+		recognition.start();
+	}
+} else {
+	alert("Can't use mic");
 }
