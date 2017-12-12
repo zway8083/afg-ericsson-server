40945f41828efca0386431f1f758e9f5b45fde83
Zway <zway8083@gmail.com>
2017-12-12 13:53:57 +0100

    Add token to /api/grade controller

--- src/main/java/server/api/GradeController.java
+++ src/main/java/server/api/GradeController.java
@@ -4,11 +4,13 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import server.database.repository.EventStatRepository;
 
@@ -18,11 +20,16 @@ public class GradeController {
 	@Autowired
 	private EventStatRepository eventStatRepository;
 
+	/* Required token */
+	@Value("${api.grade.token}")
+	private String token;
 	/* Default % gaps */
 	private static final int gaps[][] = { { 0, 60 }, { 61, 90 }, { 91, 100 } };
 
 	@GetMapping(path = "/api/grade")
-	public ResponseEntity<String> grade() {
+	public ResponseEntity<String> grade(@RequestHeader(value = "Token", required = true) String token) {
+		if (!token.equals(this.token))
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		try {
 			DateTime day = new DateTime().withMillisOfDay(0);
 			Long nEventStat = eventStatRepository.countByDate(day.toDate());
