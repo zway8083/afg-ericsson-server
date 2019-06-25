ea0ce5568ee3df33a70ca360da063b76100c2dbf
zway8083 <zway8083@gmail.com>
2019-06-25 10:39:54 +0200

    gestion plusieurs capteurs ok et email reset à 8h tout les jours

--- src/main/java/server/api/SensorEventController.java
+++ src/main/java/server/api/SensorEventController.java
@@ -65,6 +65,7 @@ public class SensorEventController {
 			event.setType(sensorType);
 			Device device = deviceRepository.findBySerial(eventRequest.getSerial());
 			event.setDevice(device);
+			event.setUser(device.getUser());
 			eventRepository.save(event);
 			return new ResponseEntity<String>(HttpStatus.CREATED);
 		} catch (Exception e) {
